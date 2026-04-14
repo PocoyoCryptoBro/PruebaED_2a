76731086ccf4708baa907fce880c01dcb68abf18
Eric <perichiquito@gmail.com>
2026-04-14 17:06:24 -0400

    implementacion test toString

---
+++ src/CursoTest.java
@@ -0,0 +1,22 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class CursoTest {
+
+    @org.junit.jupiter.api.Test
+    void getNombre() {
+        Curso curso = new Curso("Programacion");
+        assertEquals("Programacion", curso.getNombre());
+    }
+    @Test
+    void testToString() {
+        Curso curso = new Curso("Programacion");
+        Persona p = new Persona(12345678, "Juan Perez", 'H', 1, 1, 2000);
+
+        curso.aniadirAlumno(p);
+
+        String resultado = curso.toString();
+
+        assertTrue(resultado.contains("Programacion"));
+        assertTrue(resultado.contains("Juan"));
+    }
+}

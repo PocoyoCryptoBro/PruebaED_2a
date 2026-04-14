9165a1311698e381c481e63f72f0e985f2ebba66
Eric <perichiquito@gmail.com>
2026-04-14 17:12:18 -0400

    implementacion test compareTo

--- src/PersonaTest.java
+++ src/PersonaTest.java
@@ -16,4 +16,13 @@ class PersonaTest {
         Persona p2 = new Persona(12345678, "Otro Nombre", 'H', 2, 2, 2001);
 
         assertTrue(p1.equals(p2));
+    }
+
+    @Test
+    void compareToTest() {
+        Persona p1 = new Persona(11111111, "Juan Perez", 'H', 1, 1, 2000);
+        Persona p2 = new Persona(22222222, "Ana Lopez", 'M', 2, 2, 2001);
+
+        assertTrue(p1.compareTo(p2) < 0);
+    }
 }

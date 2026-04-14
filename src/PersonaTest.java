c7c8e23e850c34d05cf6e380a4436ac53dc4719f
Eric <perichiquito@gmail.com>
2026-04-14 17:10:34 -0400

    implementacion test equals

--- src/PersonaTest.java
+++ src/PersonaTest.java
@@ -9,4 +9,11 @@ class PersonaTest {
         Persona p = new Persona(12345678, "Juan Perez", 'H', 1, 1, 2000);
         assertTrue(p.getEdad() > 0);
     }
+
+    @Test
+    void equalsTest() {
+        Persona p1 = new Persona(12345678, "Juan Perez", 'H', 1, 1, 2000);
+        Persona p2 = new Persona(12345678, "Otro Nombre", 'H', 2, 2, 2001);
+
+        assertTrue(p1.equals(p2));
 }
